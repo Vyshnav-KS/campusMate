5122b130a135c1c7cdf5f676bfe31c732c7b2a62
Vyshnav-KS <62277580+Vyshnav-KS@users.noreply.github.com>
2020-10-26 11:55:01 +1345

    folders

--- script.js
+++
@@ -1,9 +0,0 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function topNavfn() {
-    var x = document.getElementById("myTopnavbar");
-    if (x.className === "topNavbar") {
-      x.className += " responsive";
-    } else {
-      x.className = "topNavbar";
-    }
-  } 
