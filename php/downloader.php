085c904d99e47a02831dd5825dc24a764465dc96
superRaptor911 <raptor.inc2018@gmail.com>
2020-11-11 20:56:09 +0530

    downloader bug fix

--- php/downloader.php
+++ php/downloader.php
@@ -12,9 +12,18 @@
   $file = $base_dir."/".$_GET["file"];
   if (!file_exists($file)) 
   {
-    $logger = new Logger();
-    $logger->addLog("Error : File $file does not exists.", 'e');
-    exit;
+    # Try absolute path
+    if (!file_exists($_GET["file"])) 
+    {
+      $logger = new Logger();
+      $logger->addLog("Error : File $file does not exists.", 'e');
+      exit;
+    }
+    else
+    {
+      # USE absolute path
+      $file = $_GET["file"];
+    }
   }
   
   $filetype = filetype($file);
